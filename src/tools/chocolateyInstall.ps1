258efc9a21d92e646a974505c82d3731a42e1173
Fabio A. Correa Duran <facorread@gmail.com>
2020-10-18 01:50:13 -0400

    1.0.1

--- src/tools/chocolateyInstall.ps1
+++ src/tools/chocolateyInstall.ps1
@@ -1,13 +1,13 @@
 $packageArgs = @{
   PackageName    = 'gpac' 
   FileType       = 'exe' 
-  Url            = 'https://download.tsi.telecom-paristech.fr/gpac/release/1.0.0/gpac-1.0.0-rev0-ga99c032b-master-win32.exe'
-  Checksum       = 'dba2b742a69e3d573a4f1497ed3027a579a6562712df900de9aa99f5d9e69f22278f4ea3f0c4a501483d00a8dbb4b8342d8dd6c29e6f02e2db6c34065d925cac'
+  Url            = 'http://download.tsi.telecom-paristech.fr/gpac/release/1.0.1/gpac-1.0.1-rev0-gd8538e8a-master-win32.exe'
+  Checksum       = '905ef2a0501a0a2fa8b350736676591c656de7779d1839c334192f3257cb8f7d23ec4e946c8b758c8e03730dcbbb2c70432846aae2123e58c48c1ceb5b595c29'
   ChecksumType   = 'sha512'
-  Url64          = 'https://download.tsi.telecom-paristech.fr/gpac/release/1.0.0/gpac-1.0.0-rev0-ga99c032b-master-x64.exe'
-  Checksum64     = '68584b740948d036366ca075b84f77a5f92aa8e885b013fa45176959103c5f6521661f7e6329d28dc2e7289fd5b180a149efeee2e1d886db54a8e0b80e63d388'
+  Url64          = 'http://download.tsi.telecom-paristech.fr/gpac/release/1.0.1/gpac-1.0.1-rev0-gd8538e8a-master-x64.exe'
+  Checksum64     = '975b08d71feb42e70eaee57aba922fc682fdbcddfbaf9418aeb91502dde166f90523fae52b03a439fe8d9d1da1666dc7d6778b7e169f0f8b3f2364a2f32aa464'
   ChecksumType64 = 'sha512'
   SilentArgs     = '/S'
 }
-  
+
 Install-ChocolateyPackage @packageArgs
