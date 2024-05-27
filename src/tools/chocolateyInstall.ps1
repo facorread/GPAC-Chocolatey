41c6078843deb13d916c6f72f07ba3d0cfa8c01a
Fabio A. Correa <facorread@gmail.com>
2024-05-27 12:50:43 -0400

    2.4.0

--- src/tools/chocolateyInstall.ps1
+++ src/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
 $packageArgs = @{
-  PackageName    = 'gpac' 
-  FileType       = 'exe' 
-  Url            = 'https://download.tsi.telecom-paristech.fr/gpac/release/2.2.1/gpac-2.2.1-rev0-gb34e3851-release-2.2-win32.exe'
-  Checksum       = '6AF54D0E79AA8151652521BDCA613012DAA10D10B4D2CDCACACDB178DB6DCEB97256284A58C97E8568E202082D0B2AE4A0E317F62473B0C55BEEDB3829A6D58C'
+  PackageName    = 'gpac'
+  FileType       = 'exe'
+  Url            = 'https://download.tsi.telecom-paristech.fr/gpac/release/2.4/gpac-2.4-rev0-g5d70253a-master-win32.exe'
+  Checksum       = '82795EFD5E1DD89826FE1CD1DCB4509F801080A19F2BBB62BACAA63A31CB91FA462A9952EF8043DACB856B706F6714C261F307277939B368E1B6C4591BBBF5B0'
   ChecksumType   = 'sha512'
-  Url64          = 'https://download.tsi.telecom-paristech.fr/gpac/release/2.2.1/gpac-2.2.1-rev0-gb34e3851-release-2.2-x64.exe'
-  Checksum64     = 'E50EACFDA1FF1F585A82687667FAF713FC4259F3223069387E3E05CE60CB08561E8FC5A278565CF1E021DD84F4F2044D9E674783B62A4F0B1AFEE667338FFD72'
+  Url64          = 'https://download.tsi.telecom-paristech.fr/gpac/release/2.4/gpac-2.4-rev0-g5d70253a-master-x64.exe'
+  Checksum64     = '2B912C761672D6903306F2702CC8CB29246F59A665418476A38BAB129BC058B10F413DB106E9A7F098FE12EB619C3CEFB7D973EF68E01CDE9148992F38A07FB4'
   ChecksumType64 = 'sha512'
   SilentArgs     = '/S'
 }
